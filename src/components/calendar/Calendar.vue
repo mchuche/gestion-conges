1e4bc95f63fe4c108d242073c37e1895de945eae
mchuche <martin.chuche@gmail.com>
2025-12-13 17:37:40 +0100

    Correction Calendar.vue : utilisation getDateKey depuis utils

--- src/components/calendar/Calendar.vue
+++ src/components/calendar/Calendar.vue
@@ -31,6 +31,7 @@ import { useQuotasStore } from '../../stores/quotas'
 import { useAuthStore } from '../../stores/auth'
 import YearViewSemester from './YearViewSemester.vue'
 import { getYear, addYears } from '../../services/dateUtils'
+import { getDateKey } from '../../services/utils'
 
 const uiStore = useUIStore()
 const leavesStore = useLeavesStore()
@@ -92,12 +93,6 @@ function handleDayMouseDown(date, event) {
   }
 }
 
-function getDateKey(date) {
-  const year = date.getFullYear()
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const day = String(date.getDate()).padStart(2, '0')
-  return `${year}-${month}-${day}`
-}
 
 onMounted(async () => {
   await loadAllData()
